474d757457b0255582f508b31fa252e43564e7d4
Jayden Kong (jjv7) <jaydenzikong@gmail.com>
2025-09-15 04:34:16 +1000

    update test cases

--- test.py
+++ test.py
@@ -4,51 +4,51 @@ from assignment2 import split_alphabets
 # split_alphabets() should output tuple[list, list]
 # represented as (lowercase, uppercase)
 
-def test_lowercase_no_duplicates():
-    inputs: list[str] = ['b', 'a', 'd', 'c']
-    expected: tuple[list, list] = (['a', 'b', 'c', 'd'], [])
+def test_single_lowercase():
+    inputs: list[str] = ['a']
+    expected: tuple[list, list] = (['a'], [])
     output: tuple[list, list] = split_alphabets(inputs)
 
     assert output == expected, f"Expected {expected}, but got {output}"
 
 def test_uppercase_no_duplicates():
-    inputs: list[str] = ['D', 'B', 'A', 'C']
-    expected: tuple[list, list] = ([], ['A', 'B', 'C', 'D'])
+    inputs: list[str] = ['A']
+    expected: tuple[list, list] = ([], ['A'])
     output: tuple[list, list] = split_alphabets(inputs)
 
     assert output == expected, f"Expected {expected}, but got {output}"
 
-def test_mixed_case_no_duplicates():
-    inputs: list[str] = ['c', 'A', 'd', 'B']
-    expected: tuple[list, list] = (['c', 'd'], ['A', 'B'])
+def test_duplicate_lowercase():
+    inputs: list[str] = ['d', 'b', 'a', 'c', 'a', 'b']
+    expected: tuple[list, list] = (['a', 'b', 'c', 'd'], [])
     output: tuple[list, list] = split_alphabets(inputs)
 
     assert output == expected, f"Expected {expected}, but got {output}"
 
-def test_case_sensitivity():
-    inputs: list[str] = ['b', 'A', 'a', 'B']
-    expected: tuple[list, list] = (['a', 'b'], ['A', 'B'])
-    output: tuple[list, list] = split_alphabets(inputs)
-
-    assert output == expected, f"Expected {expected}, but got {output}"
-
-def test_lowercase_duplicate():
-    inputs: list[str] = ['b', 'a', 'a', 'c', 'b']
-    expected: tuple[list, list] = (['a', 'b', 'c'], [])
-    output: tuple[list, list] = split_alphabets(inputs)
-
-    assert output == expected, f"Expected {expected}, but got {output}"
-
-def test_uppercase_duplicate():
+def test_duplicate_uppercase():
     inputs: list[str] = ['A', 'C', 'B', 'A', 'C']
     expected: tuple[list, list] = ([], ['A', 'B', 'C'])
     output: tuple[list, list] = split_alphabets(inputs)
 
     assert output == expected, f"Expected {expected}, but got {output}"
 
-def test_mixed_case_duplicate_case_sensitivity():
+def test_mixed_case():
+    inputs: list[str] = ['d', 'B', 'c', 'A']
+    expected: tuple[list, list] = (['c', 'd'], ['A', 'B'])
+    output: tuple[list, list] = split_alphabets(inputs)
+
+    assert output == expected, f"Expected {expected}, but got {output}"
+
+def test_case_sensitivity():
     inputs: list[str] = ['b', 'B', 'a', 'A', 'a', 'B']
     expected: tuple[list, list] = (['a', 'b'], ['A', 'B'])
+    output: tuple[list, list] = split_alphabets(inputs)
+
+    assert output == expected, f"Expected {expected}, but got {output}"
+
+def test_maximum_input():
+    inputs: list[str] = ['j', 'a', 'B', 'e', 'D', 'c', 'A', 'i', 'f', 'G', 'h', 'H', 'd', 'C', 'E', 'g', 'F', 'J', 'a', 'A']
+    expected: tuple[list, list] = (['a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'])
     output: tuple[list, list] = split_alphabets(inputs)
 
     assert output == expected, f"Expected {expected}, but got {output}"
